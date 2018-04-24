370153906bbbba8f6efbea7caff88b2e1c4e4ee6
liangmuxue <liangmuxue@gmail.com>
2018-04-24 19:11:20 +0800

    ok

--- src/services/common.js
+++ src/services/common.js
@@ -25,21 +25,10 @@ export const query = async function query(
     [options.pageNumber]: current,
   };
   // 获取通用请求头信息
-<<<<<<< HEAD
-  const { systemUser } = state.app;
-=======
   let systemUser = null;
   if (state.app) {
     ({ systemUser } = state.app);
   }
-  // 登录后获取token信息
-  // if (!systemUser) {
-  //   systemUser = {
-  //     uid: 'xxx',
-  //     token: 'xxx',
-  //   };
-  // }
->>>>>>> e2ae2b34094696895d9342fc0e60ee23ebb77fc9
   return request(endpoint, {
     method: 'get',
     filter,
