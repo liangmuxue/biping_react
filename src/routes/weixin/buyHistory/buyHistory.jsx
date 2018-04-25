4e86be251e11385e2c4ae35cb7b8a0782cadf0a4
liangmuxue <liangmuxue@gmail.com>
2018-04-25 11:16:05 +0800

    add config

---
+++ src/routes/weixin/buyHistory/buyHistory.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import mobileRouteComponent from '../../common/mobileRouteComponent';
+import { Card, WhiteSpace } from 'antd-mobile';
+import { Button, WingBlank } from 'antd-mobile';
+import 'antd-mobile/es/button/style/index.css';
+import 'antd-mobile/es/list/style/index.css';
+import InfiniteListView from '../../../components/infiniteListView';
+import { buildPagiProps } from '../../common/paginationRoute';
+import BuyCard from '../../../pageComponents/weixin/buyHistory/buyCard.jsx';
+import { rebuildBuyList } from '../../../selectors/buyList';
+import style from '../announcement/announcement.less';
+import styles from './buyHistory.less';
+/**
+ * 购买历史记录
+ * @author 赵永帅
+ * @Date2018-4-24
+ */
+const Buttongo = () => (
+  <WingBlank>
+    <Button type="primary">去看看</Button><WhiteSpace />
+  </WingBlank>
+);
+
+function genDynamics({ dispatch, accountInfo }) {
+  // const { customerName } = accountInfo;
+
+
+}
+@pureRender
+class AccountInfo extends Component {
+  constructor(props) {
+    console.log('props in AccountInfo', props);
+    super(props);
+  }
+  cardClick(msgObj) {
+    console.log('cardClick in,msgObj:', msgObj);
+  }
+  render() {
+    const { buyList } = this.props;
+    console.log('buyList is:', buyList);
+    const buyListProps = buildPagiProps(this.props.dispatch, {
+      ...buyList,
+      renderRow: (rowData, sectionID, rowID) => {
+        console.log('rowData is', rowData);
+        return (
+          <BuyCard msgObj={rowData} cardClick={this.cardClick.bind(this)} />
+        );
+      },
+    });
+    const height = document.documentElement.clientHeight;
+
+    return (
+      <div>
+        {/* 使用继承infinite的列表页组件，传递上拉加载更多的处理方法 */}
+        <InfiniteListView {...buyListProps} height={height} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  console.log('dfasdafsdsafdsafdsfd', state.buyHistory);
+  // 第一次进入时没有数据，直接返回
+  if (!state.buyHistory.dataSource) {
+    const { buyHistory } = state;
+    return { buyHistory };
+  }
+  // 加工数据
+  const buyList = rebuildBuyList(state.buyHistory);
+  return buyList;
+}
+
+export default connect(mapStateToProps)(mobileRouteComponent(AccountInfo));
+// export default mobileRouteComponent(AccountInfo);
