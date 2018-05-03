21ece62ddcf5a3c9f8832debdc2c8f33dba1e58a
zhaoys <2955905148@qq.com>
2018-05-03 21:31:58 +0800

    ereee

--- src/routes/weixin/toOpen/toOpen.jsx
+++ src/routes/weixin/toOpen/toOpen.jsx
@@ -20,6 +20,7 @@ const { CheckboxItem } = Checkbox;
 const { AgreeItem } = Checkbox;
 const WechatJSSDK = require('weixin-js-sdk');
 
+
 class toOpenDetail extends Component {
   constructor(props) {
     console.log('props in AccountInfo', props);
@@ -36,14 +37,24 @@ class toOpenDetail extends Component {
       type: 'toOpen/payTypeChange',
       payload: val,
     });
+  }
+  toPayWechat() {
+    const { params } = this.props;
+    const { commId } = this.props.toOpen;
+    console.log('toPayWechat in', params);
+    const filter = { verbId: params.typeId, commoId: commId };
     this.props.dispatch({
       type: 'toOpen/toOpenPayDetail',
       payload: filter,
     });
   }
+
   render() {
+    let isHide = true;
     console.log('toOpenDetail render', this.props);
-    const { toOpenData, backPath } = this.props.toOpen;
+    const {
+      toOpenData, backPath, selectedItem,
+    } = this.props.toOpen;
     // 如果没有数据，需要首先进行查询
     if (!toOpenData) {
       this.props.dispatch({
@@ -52,36 +63,13 @@ class toOpenDetail extends Component {
       });
       return null;
     }
-    const { data } = toOpenData;
-    const { dispatch } = this.props;
-    console.log('content in subdetail', data);
-    const dataReturn = this.props.toOpen.data;
-    if (dataReturn && dataReturn.timeStamp) {
-      const config = dataReturn;
-      console.log('config111111', config);
-      if (typeof WeixinJSBridge === 'undefined') {
-        if (document.addEventListener) {
-          alert('wechat');
-          // document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-        } else if (document.attachEvent) {
-          alert('nowechat');
-          // document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-          // document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-        }
-      } else {
-        alert('yeswechat');
-        WeixinJSBridge.invoke(
-          'getBrandWCPayRequest', {
-            config,
-          },
-          (res) => {
-            alert(res.err_msg);
-            // if (res.err_msg == 'get_brand_wcpay_request:ok') {} // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-          },
-        );
-      }
-      // WechatJSSDK.chooseWXPay();
+    if (selectedItem) {
+      console.log('selectedItem', selectedItem);
+      isHide = false;
     }
+    const { data } = toOpenData;
+    console.log('content in subdetail', data);
+
     const subDesc = `订阅${toOpenData.typeName}`;
     return (
       <div>
@@ -89,15 +77,15 @@ class toOpenDetail extends Component {
         <OpenCard openObj={this.props.systemUser} openClick={this.openClick.bind(this)} />
         {data.map(i => (
           <CheckboxItem key={i.count} onChange={() => this.switchPayType(i)} checked={i.checked}>
-            {`${i.name + i.count}元`}
+            {`${i.name + i.currentPrice / 100}元`}
           </CheckboxItem>
           ))}
         <div className={style.full} />
-        <div className={style.payBottom}>
+        <div className={isHide ? style.hide : style.payBottom}>
           <WingBlank className={style.pay}>
-            <Button type="primary" className={style.toPay}>确认支付</Button><WhiteSpace />
+            <Button type="primary" className={style.toPay} onClick={this.toPayWechat.bind(this)}>确认支付</Button><WhiteSpace />
           </WingBlank>
-          <div className={style.payMoney}>支付金额：<span className={style.sum}>20</span>元</div>
+          <div className={style.payMoney}>支付金额：<span className={style.sum}>{selectedItem}</span>元</div>
         </div>
       </div>
     );
