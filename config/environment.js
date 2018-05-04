034cbe45e2bc6b3ffb0c39606a0a84d87816f781
liangmuxue <liangmuxue@gmail.com>
2018-05-04 13:22:48 +0800

    b

--- config/environment.js
+++ config/environment.js
@@ -10,9 +10,9 @@ if (process.env.NODE_ENV === 'development') {
   // ENV.host = 'http://10.0.107.254:8280';
   // ENV.host = 'http://10.0.106.161:8280';
   // ENV.host = 'http://47.92.25.130';
-  ENV.host = 'http://10.0.109.224:8280';
+  // ENV.host = 'http://10.0.109.224:8280';
   // ENV.host = 'http://10.0.106.161:8280';
-  // ENV.host = 'http://127.0.0.1:80';
+  ENV.host = 'http://127.0.0.1:8280';
   // ENV.host = 'http://wp.test.tnb99.net';
   // ENV.imgHost = 'http://img.tnb99.cn';
   ENV.wxScanurl = 'http://localhost:4200/index.html';
