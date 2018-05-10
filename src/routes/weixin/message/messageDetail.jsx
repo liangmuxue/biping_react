c9a96ea15c4f2282cb3f12c26e8a8367d4d5f000
guliuyang <rongkun0@163.com>
2018-05-10 12:57:58 +0800

    。。。

--- src/routes/weixin/message/messageDetail.jsx
+++ src/routes/weixin/message/messageDetail.jsx
@@ -142,7 +142,7 @@ class MsgDetail extends BaseComponent {
       onClose={this.closeShare.bind(this)}
     >
       <div style={{ overflow: 'hidden' }}>
-        <img src={msgImgUrl} alt="" />
+          <img src={msgImgUrl} alt="" />
       </div>
     </Modal>);
 
