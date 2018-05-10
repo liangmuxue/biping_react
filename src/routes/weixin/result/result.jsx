72349fe94f627ac00238c6467035a57303152552
guliuyang <rongkun0@163.com>
2018-05-10 20:24:04 +0800

    。。

--- src/routes/weixin/result/result.jsx
+++ src/routes/weixin/result/result.jsx
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import WhiteSpace from 'antd-mobile/lib/white-space/index';
-import Result from 'antd-mobile/lib/result/index';
+import Button from 'antd-mobile/lib/button/index';
+import WingBlank from 'antd-mobile/lib/wing-blank/index';
 import Icon from 'antd-mobile/lib/icon/index';
 import 'antd-mobile/es/button/style/index.css';
 import 'antd-mobile/es/result/style/index.css';
 import 'antd-mobile/es/icon/style/index.css';
 import mobileRouteComponent from '../../common/mobileRouteComponent';
+import style from './result.less'
 /**
  * 老人账号信息页面
  * @author 梁慕学
  * @Date  2017-12-25
  */
-const myImg = src => <img src={src} className="spe am-icon am-icon-md" alt="" />;
 
- const ResultExample = () => (
-   <div className="result-example">
-      <Result
-      img={<Icon type="check-circle" className="spe" style={{ fill: '#1F90E6' }} />}
-    title="购买成功"
-    message="你已购买「交易所公告」3个月礼包"
-
-        />
-          <WhiteSpace />
-      </div>
-);
 
 function genDynamics({ dispatch, accountInfo }) {
   // const { customerName } = accountInfo;
 
   return (
       <div>
-<ResultExample/>
+        <div className={style.successPic}>
+          <img src="/images/result/result.png"/>
+        </div>
+        <div className={style.successful}>购买成功</div>
+        <div className={style.successTip}>你已购买「交易所公告」3个月礼包</div>
+          <div className={style.toIndex}>
+            <WingBlank>
+              <Button type="primary"  className={style.toButton} style={{width:'6.9rem' ,height:'.88rem'}}>回首页</Button><WhiteSpace />
+            </WingBlank>
+          </div>
       </div>
 
 
