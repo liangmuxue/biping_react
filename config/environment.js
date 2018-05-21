ce89a20e4e20a57a43a61291cc9b9a880ed15ff7
zhaoys <2955905148@qq.com>
2018-05-21 17:06:49 +0800

    66666

--- config/environment.js
+++ config/environment.js
@@ -9,15 +9,15 @@ if (process.env.NODE_ENV === 'development') {
   ENV.socketHost = 'http://10.0.109.126';
   // ENV.host = 'http://10.0.107.254:8280';
   // ENV.host = 'http://10.0.106.161:8280';
-  ENV.host = 'http://47.92.25.130:8280';
+  //  ENV.host = 'http://47.92.25.130:8280';
   //  ENV.host = 'http://10.0.109.224:8280';
   ENV.host = 'http://wp.test.tnb99.net';
-  ENV.host = 'http://twp.test.tnb99.net';
+  // ENV.host = 'http://twp.test.tnb99.net';
 
   // ENV.socketHost = 'http://10.0.109.221:8280';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx92489c80243c1c9c&redirect_uri=';
   ENV.messageHost = 'http://wx.test.tnb99.net';
-  ENV.messageHost = 'http://twx.test.tnb99.net';
+  // ENV.messageHost = 'http://twx.test.tnb99.net';
   // ENV.wxScanurl = 'http://localhost:4200/index.html';
   // 分享消息页面对应的网址
   ENV.msgShareUrl = 'http://localhost:9000/images/msgImages';
@@ -48,10 +48,10 @@ if (process.env.NODE_ENV === 'production') {
   ENV.resourceHost = 'http://resource.tnb99.net';
   ENV.socketHost = 'http://47.93.78.97:7090';
   ENV.host = 'http://wp.test.tnb99.net';
-  ENV.host = 'http://twp.test.tnb99.net';
+  //  ENV.host = 'http://twp.test.tnb99.net';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx92489c80243c1c9c&redirect_uri=';
   ENV.messageHost = 'http://wx.test.tnb99.net';
-  ENV.messageHost = 'http://twx.test.tnb99.net';
+  //  ENV.messageHost = 'http://twx.test.tnb99.net';
   ENV.imgHost = 'http://img.tnb99.cn';
   ENV.imgUrl = 'https://biping.oss-cn-beijing.aliyuncs.com';
   ENV.wxScanurl = 'http://localhost:4200/index.html';
