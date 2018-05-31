5fd004dc33bfcb1694d65b6f03fc4813930ee042
liangmuxue <liangmuxue@gmail.com>
2018-05-31 14:15:55 +0800

    random

--- src/routes/weixin/enterGroup/enterGroup.jsx
+++ src/routes/weixin/enterGroup/enterGroup.jsx
@@ -79,7 +79,7 @@ class Join extends BaseComponent {
     }
     console.log('enterGroup44444', this.props);
     // 分享消息的图片链接
-    const wechatImg = `${imgUrl}/Static/images/wechat/enterGroup.png`;
+    const wechatImg = `${imgUrl}/Static/images/wechat/enterGroup.png?${Math.random()}`;
     const modal = (<Modal
       visible={showShare}
       transparent
