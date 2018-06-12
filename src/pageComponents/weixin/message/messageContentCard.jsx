5818b580ad4ff9e6329783859191072cf6d52013
guliuyang <rongkun0@163.com>
2018-06-12 13:15:11 +0800

    。。

--- src/pageComponents/weixin/message/messageContentCard.jsx
+++ src/pageComponents/weixin/message/messageContentCard.jsx
@@ -82,7 +82,7 @@ class MessageContent extends React.Component {
             <div className={style.tableTitle}>
               <div className={style.tableTime}> {moment(msg.qTime * 1000).format('HH:mm')}</div>
               <div className={style.tablePrice}>{msg.price.toString().substr(0, 9)}</div>
-              <div className={style.tableChg}><span className={msg.gainDiffer >= '0' ? style.toUp : style.toDown}><span>{msg.gainDiffer >= '0' ? '+': '' }</span>{msg.gainDiffer ? (msg.gainDiffer * 100).toString().substr(0, 4) +'%' : '-' }</span></div>
+              <div className={style.tableChg}><span className={msg.gainDiffer >= '0' ? style.toUp : style.toDown}><span>{msg.gainDiffer > 0 ? '+' :' '}</span>{msg.gainDiffer ? (msg.gainDiffer * 100).toString().substr(0, 4) +'%' : '-' }</span></div>
               <div className={style.tableIncome}>{msg.gainHold ? msg.gainHold.toString().substr(0, 8) : msg.gainHold}</div>
             </div>
           </div>
