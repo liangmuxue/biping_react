8b6ac2f288625592b3c4e450c40410a0d9b882c5
liangmuxue <liangmuxue@gmail.com>
2018-06-25 19:37:28 +0800

    取消showhide模式

--- config/environment.js
+++ config/environment.js
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === 'development') {
   // ENV.host = 'http://10.0.106.10:8280';
   // ENV.host = 'http://localhost:8280';
   // ENV.host = 'http://10.0.107.231:8280';
-  ENV.host = 'http://10.0.108.137:8280';
+  // ENV.host = 'http://10.0.108.137:8280';
   // ENV.host = 'http://wp.bipingcoin.com';
-
+  ENV.host = 'http://twp.test.tnb99.net';
   // ENV.socketHost = 'http://10.0.109.221:8280';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx92489c80243c1c9c&redirect_uri=';
   ENV.messageHost = 'http://twx.test.tnb99.net';
