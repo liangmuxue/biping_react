04401806f7dbe267fa00c684bc2d14768a3b58c1
MaoDapeng <Maodp@infogloble.com>
2018-06-30 20:37:23 +0800

    share fix

--- config/environment.js
+++ config/environment.js
@@ -12,8 +12,8 @@ if (process.env.NODE_ENV === 'development') {
   // ENV.host = 'http://localhost:8280';
   // ENV.host = 'http://10.0.107.231:8280';
   // ENV.host = 'http://10.0.108.137:8280';
-  // ENV.host = 'http://twp.test.tnb99.net';
-  ENV.host = 'https://wps.bipingcoin.com';
+  ENV.host = 'http://twp.test.tnb99.net';
+  // ENV.host = 'https://wps.bipingcoin.com';
 
   // ENV.socketHost = 'http://10.0.109.221:8280';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx92489c80243c1c9c&redirect_uri=';
