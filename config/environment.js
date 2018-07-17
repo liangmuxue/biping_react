dcb0a2708fc343002f8a06ead483e10e3cfc6a0e
liangmuxue <liangmuxue@gmail.com>
2018-07-17 15:39:40 +0800

    g

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'test') {
   ENV.host = 'https://twp.closerhearts.net.cn';
   //  ENV.host = 'http://twp.closerhearts.net.cn';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxfd4795144c135bdd&redirect_uri=';
-  ENV.messageHost = 'http://twx.closerhearts.net.cn';
+  ENV.messageHost = 'https://twx.closerhearts.net.cn';
   ENV.qrImgName = 'wxtest.png';
   //  ENV.messageHost = 'http://twx.closerhearts.net.cn';
   ENV.imgHost = 'http://img.tnb99.cn';
@@ -52,14 +52,14 @@ if (process.env.NODE_ENV === 'test') {
 if (process.env.NODE_ENV === 'production') {
   ENV.resourceHost = 'http://resource.tnb99.net';
   ENV.socketHost = 'http://47.93.78.97:7090';
-  ENV.host = 'http://wps.bipingcoin.com';
+  ENV.host = 'https://wps.bipingcoin.com';
   //  ENV.host = 'http://twp.closerhearts.net.cn';
   ENV.wechatHost = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx92489c80243c1c9c&redirect_uri=';
-  ENV.messageHost = 'http://wx.bipingcoin.com';
+  ENV.messageHost = 'https://wx.bipingcoin.com';
   ENV.qrImgName = 'bipingcoin.png';
   //  ENV.messageHost = 'http://twx.closerhearts.net.cn';
   ENV.imgHost = 'http://img.tnb99.cn';
-  ENV.imgUrl = 'http://biping.oss-cn-beijing.aliyuncs.com';
+  ENV.imgUrl = 'https://biping.oss-cn-beijing.aliyuncs.com';
   ENV.wxScanurl = 'http://localhost:4200/index.html';
   ENV.imgShareUrl = 'http://img.closerhearts.net.cn';
   ENV.wxBrowserCheck = true;
