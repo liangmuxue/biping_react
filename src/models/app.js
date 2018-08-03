0ced7612f544edf636144154d9730c31aa2a5f9c
liangmuxue <liangmuxue@gmail.com>
2018-08-03 10:47:47 +0800

    c

--- src/models/app.js
+++ src/models/app.js
@@ -187,6 +187,10 @@ const App = {
       } else if (success && response.flag === 1003) {
         // 取消关注了的用户不允许直接进入
         yield put({ type: 'tourLogin', payload: { attentionModal: true } });
+        if (!mockUser) {
+          window.localStorage.clear();
+          console.log(`need clear`);
+        }
       } else if (success && response.flag === 0 && !response.data) {
         // 用户密码登录失败,重置缓存
         const { mockUser } = config.env;
