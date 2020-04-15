473c9c220ca51468051e94c0a639926b51e78c0b
RK <rongkun0@163.com>
2020-04-15 14:23:26 +0800

    完美谢幕

--- .webpackrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const { version } = require('./package.json')
-export default {
-  define: {
-    'process.env.contextPath': process.env.contextPath,
-  },
-  entry: 'src/index.js',
-  outputPath: `./dist/${version}`,
-};
